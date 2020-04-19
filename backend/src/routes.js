ddb5eef0d34fbe9302979bed574bdfeed555547c
vonnerk2 <renanbizarri@hotmail.com>
2020-04-19 19:45:19 -0300

    criptografia da senha do usuario e começo do login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routers = new Router();
 
 const UserController = require("./controllers/UserController");
 
-routers.post("/create", UserController.createUser);
+routers.post("/createAccount", UserController.createUser);
+routers.post("/login", UserController.login);
 
 module.exports = routers;
