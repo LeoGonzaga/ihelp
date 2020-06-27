bafd8c7bbc106e5d9e55d7d234f467269833f39a
Leonardo Gonzaga <sacredlotus05@gmail.com>
2020-06-27 16:00:07 -0300

    login ok

--- MyApp/src/Views/Login/index.tsx
+++ MyApp/src/Views/Login/index.tsx
@@ -50,12 +50,40 @@ const style = StyleSheet.create({
 });
 
 const Login: React.FC = ({navigation}) => {
-  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
+
+  const loginUser = async (email: string, password: string) => {
+    try {
+      if (email == '' || password == '') {
+        Alert.alert(
+          'Ops!',
+          'Por favor, preencha todos os campos para continuar',
+        );
+      } else {
+        let response = await fetch('https://ihelp-back.herokuapp.com/login', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({email, password}),
+        });
+        let resJSON = await response.json();
+        console.log('resLogin', resJSON);
+        if (resJSON.message) {
+          Alert.alert('Ops!', resJSON.message);
+        } else {
+          navigation.navigate('Home');
+        }
+      }
+    } catch (error) {
+      console.log('Error login', error);
+    }
+  };
 
   return (
     <View style={style.container}>
@@ -67,6 +95,7 @@ const Login: React.FC = ({navigation}) => {
 
           <TextInput
             placeholder="Digite seu email"
+            autoCapitalize="none"
             style={[style.input, style.layout]}
             onChangeText={(value) => setEmail(value)}></TextInput>
           <TextInput
@@ -78,7 +107,7 @@ const Login: React.FC = ({navigation}) => {
           <TouchableOpacity
             style={[style.button, style.layout, {marginTop: 40}]}
             onPress={() => {
-              navigation.navigate('Home');
+              loginUser(email, password);
             }}>
             <Text style={[style.text, {fontSize: 20, margin: 5}]}>Entrar</Text>
           </TouchableOpacity>
